3c5e1891a3d8b3fcc4253c1d6e695d881fad4e93
Russell <75209707+rustyff12@users.noreply.github.com>
2025-09-01 11:39:16 +0200

    Hero layout mobile

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,7 +1,65 @@
+import { useState } from "react";
 export default function Calculator() {
+  const [units, setUnits] = useState("metric");
+  const [inputComplete, setInputComplete] = useState(false);
   return (
-    <article className="claculator">
-      <form>{/*  */}</form>
+    <article className="calculator">
+      <form>
+        <h3>Enter your details below</h3>
+
+        <fieldset id="units" className="units">
+          <legend className="sr-only">Units</legend>
+
+          <label htmlFor="metric" className="radio-label">
+            <input
+              className="radio-input"
+              type="radio"
+              name="units"
+              id="metric"
+              value="metric"
+            />
+            Metric
+          </label>
+
+          <label htmlFor="imperial" className="radio-label">
+            <input
+              className="radio-input"
+              type="radio"
+              name="units"
+              id="imperial"
+              value="imperial"
+            />
+            Imperial
+          </label>
+        </fieldset>
+        <div className="inputs">
+          <div className="input-container">
+            <label htmlFor="height">Height</label>
+            <input type="text" />
+            {/* <input type="text" /> */}
+          </div>
+          <div className="input-container">
+            <label htmlFor="height">Weight</label>
+            <input type="text" />
+            {/* <input type="text" /> */}
+          </div>
+        </div>
+      </form>
+      <div className="result-container">
+        {inputComplete ? (
+          <div>
+            <p className="result-header">YourBMI is...</p>
+          </div>
+        ) : (
+          <div>
+            <h3>Welcome!</h3>
+            <p>
+              Enter your height and weight and you&apos;ll see your BMI result
+              here
+            </p>
+          </div>
+        )}
+      </div>
     </article>
   );
 }
