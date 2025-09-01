07e343d6c3d30b8b31aa1c7fe868822deff8ffb6
Russell <75209707+rustyff12@users.noreply.github.com>
2025-09-01 17:37:31 +0200

    Info layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
+import Info from "./components/Info";
 function App() {
   return (
-    <>
+    <div className="site-wrapper">
       <Hero />
-    </>
+      <Info />
+    </div>
   );
 }
 
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,7 +1,25 @@
 export default function Info() {
   return (
-    <>
-      <h2>Info goes here</h2>
-    </>
+    <section className="info">
+      <div className="info-content">
+        <img
+          className="info-image"
+          src="/image-man-eating.webp"
+          alt="Man eating Sushi"
+        />
+        <div className="info-content-text">
+          <h2>What your BMI result means</h2>
+          <p>
+            A BMI range of 18.5 to 24.9 is considered a 'healthy weight.'
+            Maintaining a healthy weight may lower your chances of experiencing
+            health issues later on, such as obesity and type 2 diabetes. Aim for
+            a nutritious diet with reduced fat and sugar content, incorporating
+            ample fruits and vegetables. Additionally, strive for regular
+            physical activity, ideally about 30 minutes daily for five days a
+            week.
+          </p>
+        </div>
+      </div>
+    </section>
   );
 }
